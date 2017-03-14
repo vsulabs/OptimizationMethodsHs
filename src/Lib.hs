e1f75123a52a228499f8973d6a987b208e4eac62
Diadlo <polsha3@gmail.com>
2017-03-14 10:53:59 +0300

    Using corage for interval params

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,8 +4,8 @@ module Lib
     ( someFunc
     ) where
 
-findNext :: (Fractional a, Ord b) => a -> (a -> b) -> a -> a -> (a, a)
-findNext d f a b 
+findNext :: (Fractional a, Ord b) => a -> (a -> b) -> (a, a) -> (a, a)
+findNext d f (a, b) 
         | f1 <  f2 = (a,  u2)
         | f1 >  f2 = (u1, b)
         | f1 == f2 = (u1, u2)
@@ -15,18 +15,18 @@ findNext d f a b
            f1 = f u1
            f2 = f u2
 
-binSearch :: (Fractional a, Ord a, Ord b) => a -> (a -> b) -> a -> a -> a
-binSearch d f a b
+binSearch :: (Fractional a, Ord a, Ord b) => a -> (a -> b) -> (a, a) -> a
+binSearch d f (a, b)
         | b - a < eps  = (a + b) / 2
-        | otherwise    = binSearch d f a1 b1
+        | otherwise    = binSearch d f next
         where 
-          (a1, b1) = findNext d f a b
+          next = findNext d f (a, b)
           eps = 0.001
 
 f :: Double -> Double
 f x = (x-2)*(x-4)
           
 someFunc :: IO ()
-someFunc = putStrLn(show(bs f (-5.0) 5.0))
+someFunc = putStrLn(show(bs f (-5.0, 5.0)))
   where 
     bs = binSearch 0.000001
